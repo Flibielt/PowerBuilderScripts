a706aea747ccffcba22df1e2075f71a53d315486
Flibielt <hcsabi98@gmail.com>
2020-02-14 15:12:12 +0100

    Bug fixes

--- replace.py
+++ replace.py
@@ -29,38 +29,38 @@ for root, dirs, files in os.walk("..\\mc2svn17UD", topdown=False):
                     line = line.replace("u_gomb", "u_dynamic_button")
                     uGomb = True
                     uGombEvents = False
-                elif uGomb and "end type" in line:
+                elif "end type" in line:
                     uGomb = False
-                    if len(setterFuncs) > 0:
-                        setterInsertion = SetterInsertion.wait
+                    uGombEvents = True
+                    setterInsertion = SetterInsertion.wait
                 #Convert some properties to setters
                 if uGomb:
                     if "int textsize" in line.lower() or "integer textsize" in line.lower():
-                        setterFuncs.add("this.set_textsize(" + line[line.find["= ":]].strip() + ")")
+                        setterFuncs.add("this.set_textsize(" + line[line.find("= "):].strip() + ")")
                         skipLine = True
-                    elif "string Text " in line.lower():
-                        setterFuncs.add("this.set_text(" + line[line.find["= ":]].strip() + ")")
+                    elif "string text " in line.lower():
+                        setterFuncs.add("this.set_text(" + line[line.find("= "):].strip() + ")")
                         skipLine = True
                     elif "fontcharset fontcharset" in line.lower():
-                        setterFuncs.add("this.set_fontcharset(" + line[line.find["= ":]].strip() + ")")
+                        setterFuncs.add("this.set_fontcharset(" + line[line.find("= "):].strip() + ")")
                         skipLine = True
                     elif "fontfamily fontfamily" in line.lower():
-                        setterFuncs.add("this.set_fontfamily(" + line[line.find["= ":]].strip() + ")")
+                        setterFuncs.add("this.set_fontfamily(" + line[line.find("= "):].strip() + ")")
                         skipLine = True
                     elif "fontpitch fontpitch" in line.lower():
-                        setterFuncs.add("this.set_fontpitch(" + line[line.find["= ":]].strip() + ")")
+                        setterFuncs.add("this.set_fontpitch(" + line[line.find("= "):].strip() + ")")
                         skipLine = True
                     elif "string facename" in line.lower():
-                        setterFuncs.add("this.set_facename(" + line[line.find["= ":]].strip() + ")")
+                        setterFuncs.add("this.set_facename(" + line[line.find("= "):].strip() + ")")
                         skipLine = True
                     elif "integer weight" in line.lower() or "int weight" in line.lower():
-                        setterFuncs.add("this.set_weight(" + line[line.find["= ":]].strip() + ")")
+                        setterFuncs.add("this.set_weight(" + line[line.find("= "):].strip() + ")")
                         skipLine = True
                     elif "string powertiptext" in line.lower():
-                        setterFuncs.add("this.add_powertip(" + line[line.find["= ":]].strip() + ")")
+                        setterFuncs.add("this.add_powertip(" + line[line.find("= "):].strip() + ")")
                         skipLine = True
                     elif "string tag" in line.lower():
-                        setterFuncs.add("this.set_tag(" + line[line.find["= ":]].strip() + ")")
+                        setterFuncs.add("this.set_tag(" + line[line.find("= "):].strip() + ")")
                         skipLine = True
                     elif "boolean default" in line.lower():
                         defaultButtons.add("button name")
@@ -77,11 +77,12 @@ for root, dirs, files in os.walk("..\\mc2svn17UD", topdown=False):
                 if uGombEvents:
                     if "event clicked;call super::clicked;" in line.lower():
                         line = line.replace("event clicked;call super::clicked;", "event u_click;call super::u_click;")
-                        setterInsertion = SetterInsertion.insert
                     elif "event constructor;call super::constructor;" in line.lower():
                         setterInsertion = SetterInsertion.insert
-                    elif line.startswith("type") and setterInsertion == SetterInsertion.wait:
+                    elif line.strip().startswith("type") and setterInsertion == SetterInsertion.wait:
                         setterInsertion = SetterInsertion.witchConstructor
+                    elif line.strip().startswith("type"):
+                        uGombEvents = False
 
                 if not skipLine:
                     with open(outFile, "a") as f:
@@ -90,18 +91,23 @@ for root, dirs, files in os.walk("..\\mc2svn17UD", topdown=False):
                             f.write("\n")
                             for setter in setterFuncs:
                                 f.write(setter)
+                                f.write("\r")
                             setterFuncs.clear()
                             setterInsertion = SetterInsertion.done
                         elif setterInsertion == SetterInsertion.witchConstructor:
                             f.write("event constructor;call super::constructor;")
+                            f.write("\r")
                             for setter in setterFuncs:
                                 f.write(setter)
+                                f.write("\r")
                             f.write("end event")
-                            f.write("\n")
+                            f.write("\r")
+                            f.write("\r")
                             f.write(line.rstrip())
-                            f.write("\n")
+                            f.write("\r")
                             setterFuncs.clear()
                             setterInsertion = SetterInsertion.done
                         else:
                             f.write(line.rstrip())
                             f.write("\n")
+                skipLine = False
