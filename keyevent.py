35acfed5f9d56955b23745f8116710cf626ad21c
Flibielt <hcsabi98@gmail.com>
2020-02-20 16:05:21 +0100

    Defined insert area

--- keyevent.py
+++ keyevent.py
@@ -13,16 +13,20 @@ for root, dirs, files in os.walk("..\\medikai", topdown=False):
         os.makedirs(os.path.dirname(outFile), exist_ok=True)
         with codecs.open(fileName, encoding='utf8') as f:
             for line in f:
-                if "from u_gomb within" in line or "from commandbutton within" in line or "from u_ok_gomb within" in line or "from u_megsem_gomb within" in line or "from w_adatbevalap`cb_" in line:
+                if "from u_dynamic_button within" in line:
                     buttons.add(line[line.find("type ") + 4:line.find(" from")].strip())
-                    buttonProp = True
-                elif buttonProp:
-                    if "end type" in line:
-                        buttonProp = False
-                    else:
-                        if "default" in line:
-                            
-                
+        
+        with codecs.open(fileName, encoding='utf8') as f:
+            endCount = 0
+            for line in f:
+                with open(outFile, "a") as f:
+                    if "end on" in line:
+                        endCount = endCount + 1
+                    
+                    if endCount == 2:
+                        print("Insert key event")
+
+
 
 for button in buttons:
     print(button)
