0ab7b2925f8ea42d3862313e448cdda962cab518
Flibielt <hcsabi98@gmail.com>
2020-02-20 11:03:51 +0100

    Bug fixes in the replace script

--- replace.py
+++ replace.py
@@ -23,7 +23,7 @@ setterFuncs = {""}
 #todo: cb_ok.Event Post Clicked() helyett cb_ok.Event Post u_click(1, 1, 1)
 #todo: width és height property legyen mindenhol
 
-for root, dirs, files in os.walk("..\\mc2svn17UD", topdown=False):
+for root, dirs, files in os.walk("..\\medikai", topdown=False):
    for name in files:
       if name.startswith("w_"):
         fileName = os.path.join(root, name)
@@ -33,16 +33,11 @@ for root, dirs, files in os.walk("..\\mc2svn17UD", topdown=False):
         with codecs.open(fileName, encoding='utf8') as f:
             buttons.clear()
             for line in f:
-                if "from u_gomb within" in line or "from commandbutton within" in line or "from u_ok_gomb within" in line or "from u_megsem_gomb within" in line:
+                if "from u_gomb within" in line or "from commandbutton within" in line or "from u_ok_gomb within" in line or "from u_megsem_gomb within" in line or "from w_adatbevalap`cb_" in line:
                     line = line.replace("u_gomb", "u_dynamic_button")
                     line = line.replace("commandbutton", "u_dynamic_button")
                     line = line.replace("u_ok_gomb", "u_dynamic_button")
                     line = line.replace("u_megsem_gomb", "u_dynamic_button")
-                    if line in buttons:
-                        if "from u_gomb within" in line or "from commandbutton within" in line or "from u_ok_gomb within" in line or "from u_megsem_gomb within" in line:
-                            setterFuncs.add("this.resize_inner_objects(this.width, this.height)")
-                    else:
-                        uGombDeclaration = False
                     buttons.add(line)
                     uGomb = True
                     uGombEvents = False
@@ -56,53 +51,59 @@ for root, dirs, files in os.walk("..\\mc2svn17UD", topdown=False):
                 elif uGomb:
                     if "int textsize" in line.lower() or "integer textsize" in line.lower():
                         setterFuncs.add("this.set_textsize(" + line[line.find("=") + 1:].strip() + ")")
+                        setterFuncs.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
                     elif "string text" in line.lower() or "string Text" in line:
                         setterFuncs.add("this.set_text(" + line[line.find("=") + 1:].strip() + ")")
+                        setterFuncs.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
                     elif "fontcharset fontcharset" in line.lower():
                         setterFuncs.add("this.set_fontcharset(" + line[line.find("=") + 1:].strip() + ")")
+                        setterFuncs.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
                     elif "fontfamily fontfamily" in line.lower():
                         setterFuncs.add("this.set_fontfamily(" + line[line.find("=") + 1:].strip() + ")")
+                        setterFuncs.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
                     elif "fontpitch fontpitch" in line.lower():
                         setterFuncs.add("this.set_fontpitch(" + line[line.find("=") + 1:].strip() + ")")
+                        setterFuncs.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
                     elif "string facename" in line.lower():
                         setterFuncs.add("this.set_facename(" + line[line.find("=") + 1:].strip() + ")")
+                        setterFuncs.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
                     elif "integer weight" in line.lower() or "int weight" in line.lower():
                         setterFuncs.add("this.set_weight(" + line[line.find("=") + 1:].strip() + ")")
+                        setterFuncs.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
                     elif "string powertiptext" in line.lower():
                         setterFuncs.add("this.add_powertip(" + line[line.find("=") + 1:].strip() + ")")
+                        setterFuncs.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
                     elif "string tag" in line.lower():
                         setterFuncs.add("this.set_tag(" + line[line.find("=") + 1:].strip() + ")")
+                        setterFuncs.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
                     elif "boolean default" in line.lower():
                         setterFuncs.add("this.set_default(" + line[line.find("=") + 1:].strip() + ")")
+                        setterFuncs.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
                     elif "boolean cancel" in line.lower():
                         setterFuncs.add("this.set_cancel(" + line[line.find("=") + 1:].strip() + ")")
+                        setterFuncs.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
                     elif "boolean ib_ugomb2" in line.lower():
+                        setterFuncs.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
-                    elif "width" in line.lower():
-                        widthChange = True
-                    elif "height" in line.lower():
-                        heightChange = True
-                        #More functionality?
-                
+                    
                 if uGombEvents:
                     if "event clicked;call super::clicked;" in line.lower():
                         line = line.replace("event clicked;call super::clicked;", "event u_click;call super::u_click;")
                     elif "event clicked;" in line.lower():
                         line = line.replace("event clicked;", "event u_click;call super::u_click;")
-                    elif "event constructor;call super::constructor;" in line.lower() and uGombDeclaration:
+                    elif "event constructor;call super::constructor;" in line.lower():
                         setterInsertion = SetterInsertion.insert
-                        uGombDeclaration = False
                     elif line.strip().startswith("type") and setterInsertion == SetterInsertion.wait:
                         setterInsertion = SetterInsertion.witchConstructor
                         uGombEvents = False
@@ -127,19 +128,20 @@ for root, dirs, files in os.walk("..\\mc2svn17UD", topdown=False):
                                 f.write("\r")
                             setterFuncs.clear()
                             setterInsertion = SetterInsertion.done
-                        elif setterInsertion == SetterInsertion.witchConstructor and uGombDeclaration:
-                            f.write("event constructor;call super::constructor;")
-                            f.write("\r")
-                            for setter in setterFuncs:
-                                f.write(setter)
+                        elif setterInsertion == SetterInsertion.witchConstructor and line.lower().startswith("type"):
+                            if len(setterFuncs) > 1:
+                                f.write("event constructor;call super::constructor;")
                                 f.write("\r")
-                            f.write("end event")
-                            f.write("\r")
-                            f.write("\n")
+                                for setter in setterFuncs:
+                                    f.write(setter)
+                                    f.write("\r")
+                                f.write("end event")
+                                f.write("\r")
+                                f.write("\n")
+                                setterFuncs.clear()
+                            setterInsertion = SetterInsertion.done
                             f.write(line.rstrip())
                             f.write("\r")
-                            setterFuncs.clear()
-                            setterInsertion = SetterInsertion.done
                         else:
                             f.write(line.rstrip())
                             f.write("\n")
