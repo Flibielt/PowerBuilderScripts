865c25d17abba80bff2db6f87374226e84cfe241
Flibielt <hcsabi98@gmail.com>
2020-02-28 09:19:30 +0100

    List all the files, which has property modifiers

--- modifiers.py
+++ modifiers.py
@@ -1,10 +1,22 @@
 import codecs
 import os
 
+modifiedTypes = {""}
+filesAffected = {""}
+prop = False
+
 for root, dirs, files in os.walk("..\\medikai", topdown=False):
    for name in files:
         fileName = os.path.join(root, name)
-        print(fileName)
-        with codecs.open(fileName, encoding='utf8') as f:
-            for line in f:
-                print(repr(line))
+        #print(fileName)
+        modifiedTypes.clear()
+        if "d_" not in fileName:
+            with codecs.open(fileName, encoding='utf8') as f:
+                for line in f:
+                    if ".width" in line.lower() or ".height" in line.lower() or ".x" in line.lower() or ".y" in line.lower() or ".textsize" in line.lower():
+                        filesAffected.add(fileName)
+                        #print(line.strip())
+                
+for f in filesAffected:
+    print(f)
+print("Files affected: " + str(len(filesAffected)))
