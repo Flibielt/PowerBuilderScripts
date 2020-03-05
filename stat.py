4c588d58fd4267b1d136dbba604ff7df4d1b095e
Flibielt <hcsabi98@gmail.com>
2020-03-05 09:01:27 +0100

    Count files, lines, single and multi line edits

--- stat.py
+++ stat.py
@@ -4,10 +4,12 @@ import os
 singleEdit = 0
 multiEdit = 0
 lineCount = 0
+fileCount = 0
 
 for root, dirs, files in os.walk("..\\mc2svn17UD", topdown=False):
    for name in files:
       if not ".svn" in root:
+         fileCount = fileCount + 1
          fileName = os.path.join(root, name)
          print(fileName)
          with codecs.open(fileName, encoding='utf8') as f:
@@ -17,3 +19,9 @@ for root, dirs, files in os.walk("..\\mc2svn17UD", topdown=False):
                     multiEdit = multiEdit + 1
                 elif "from u_sledit" in line or "from u_sledit_bekuldokod" in line or "from u_sledit_dwfind" in line:
                     singleEdit = singleEdit + 1
+
+print("File count: " + str(fileCount))
+print("Line count: " + str(lineCount))
+print("SingleLineEdit count: " + str(singleEdit))
+print("MultilineEdit count: " + str(multiEdit))
+
