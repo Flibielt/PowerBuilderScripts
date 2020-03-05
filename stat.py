e6b5adcd82ff0152e5666f913d5322899619a26b
Flibielt <hcsabi98@gmail.com>
2020-03-05 08:55:27 +0100

    Create statistics from the code

---
+++ stat.py
@@ -0,0 +1,19 @@
+import codecs
+import os
+
+singleEdit = 0
+multiEdit = 0
+lineCount = 0
+
+for root, dirs, files in os.walk("..\\mc2svn17UD", topdown=False):
+   for name in files:
+      if not ".svn" in root:
+         fileName = os.path.join(root, name)
+         print(fileName)
+         with codecs.open(fileName, encoding='utf8') as f:
+            for line in f:
+                lineCount = lineCount + 1
+                if "from u_multilinedit" in line:
+                    multiEdit = multiEdit + 1
+                elif "from u_sledit" in line or "from u_sledit_bekuldokod" in line or "from u_sledit_dwfind" in line:
+                    singleEdit = singleEdit + 1
