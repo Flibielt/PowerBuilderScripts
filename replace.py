18eeae409091de0f8ea046e5bd495417550a7301
Flibielt <hcsabi98@gmail.com>
2020-03-05 12:33:23 +0100

    Change the default text to DINPro-Medium

--- replace.py
+++ replace.py
@@ -28,7 +28,7 @@ for root, dirs, files in os.walk("..\\medikai", topdown=False):
       if name.startswith("w_"):
         fileName = os.path.join(root, name)
         print(fileName)
-        outFile = "..\\out" + fileName[fileName.find("\\"):]
+        outFile = "..\\out2" + fileName[fileName.find("\\"):]
         os.makedirs(os.path.dirname(outFile), exist_ok=True)
         with codecs.open(fileName, encoding='utf8') as f:
             for line in f:
@@ -51,6 +51,8 @@ for root, dirs, files in os.walk("..\\medikai", topdown=False):
                     uGomb = False
                     uGombEvents = True
                     setterInsertion = SetterInsertion.wait
+                elif "string facename" in line.lower():
+                    line = line[:line.find("\"")] +  "\"DINPro-Medium\""
                 #Convert some properties to setters
                 elif uGomb:
                     if "int textsize" in line.lower() or "integer textsize" in line.lower():
