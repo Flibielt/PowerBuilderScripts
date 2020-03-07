a234c1e5debfda2c1adf880a8d41a3064dfa1778
Csaba Hermann <hcsabi98@gmail.com>
2020-03-07 16:12:59 +0100

    Write files

--- replace2.py
+++ replace2.py
@@ -6,7 +6,7 @@ for root, dirs, files in os.walk("folderName", topdown=False):
       if name.startswith("w_") or name.startswith("u"):
         fileName = os.path.join(root, name)
         print(fileName)
-        outFile = "..\\out2" + fileName[fileName.find("\\"):]
+        outFile = "..\\out3" + fileName[fileName.find("\\"):]
         os.makedirs(os.path.dirname(outFile), exist_ok=True)
         dynamicButton = False
         with codecs.open(fileName, encoding='utf8') as f:
@@ -21,3 +21,7 @@ for root, dirs, files in os.walk("folderName", topdown=False):
                 
                 if "u_dynamic_button2" in line:
                     line.replace("u_dynamic_button2", "u_dynamic_button")
+                
+                with open(outFile, "a") as f:
+                    f.write(line.rstrip())
+                    f.write("\n")
