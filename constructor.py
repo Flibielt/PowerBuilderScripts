4adc1fdc1c1f28f64e6f5fea2f785925998e544a
Flibielt <hcsabi98@gmail.com>
2020-03-09 12:02:15 +0100

    Bug fix

--- constructor.py
+++ constructor.py
@@ -14,34 +14,43 @@ for root, dirs, files in os.walk("proba", topdown=False):
         outFile = "out" + fileName[fileName.find("\\"):]
         os.makedirs(os.path.dirname(outFile), exist_ok=True)
         dynamicButton = False
+        skipLine = False
         with codecs.open(fileName, encoding='utf8') as f:
             for line in f:
                 if " from u_dynamic_button within ":
                     dynamicButton = True
                     dynamicButtonProp = True
+                    constProp.add("this.resize_inner_objects(this.width, this.height)")
                 elif " cb_" in line.lower() or " pb_" in line.lower():
                     if "`" in line.lower():
                         dynamicButton = True
                         dynamicButtonProp = True
+                        constProp.add("this.resize_inner_objects(this.width, this.height)")
                 
                 if dynamicButtonProp == True:
                     if "textsize" in line.lower() and "=" in line:
                         constProp.add("this.set_textsize(" + line[line.find("=") + 1:].strip() + ")")
+                        constProp.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
                     elif "string powertiptext" in line.lower() and "=" in line:
                         constProp.add("this.add_powertip(" + line[line.find("=") + 1:].strip() + ")")
+                        constProp.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
                     elif "string text" in line.lower() and "=" in line:
                         constProp.add("this.set_text(" + line[line.find("=") + 1:].strip() + ")")
+                        constProp.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
                     elif "string tag" in line.lower():
                         constProp.add("this.set_tag(" + line[line.find("=") + 1:].strip() + ")")
+                        constProp.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
                     elif "boolean default" in line.lower():
                         constProp.add("this.set_default(" + line[line.find("=") + 1:].strip() + ")")
+                        constProp.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
                     elif "boolean cancel" in line.lower():
                         constProp.add("this.set_cancel(" + line[line.find("=") + 1:].strip() + ")")
+                        constProp.add("this.resize_inner_objects(this.width, this.height)")
                         skipLine = True
                     elif "end type" in line.lower():
                         dynamicButtonProp = False
@@ -58,15 +67,13 @@ for root, dirs, files in os.walk("proba", topdown=False):
                                 insertSetter = True
                         elif dynamicButton == False and len(constProp) > 1:
                             insertConst = True
-                        
-                        if insertSetter == True:
+                        elif insertSetter == True:
                             f.write(line.rstrip())
                             f.write("\n")
                             for settter in constProp:
                                 f.write(settter)
                                 f.write("\n")
                             constProp.clear()
-
                         elif insertConst == True:
                             f.write("event constructor;call super::constructor;")
                             f.write("\n")
@@ -77,6 +84,9 @@ for root, dirs, files in os.walk("proba", topdown=False):
 
                             f.write("end event")
                             f.write("\n")
+                            f.write("\n")
+                        else:
+                            f.write(line.rstrip())
                             f.write("\n")
                 skipLine = False
 
