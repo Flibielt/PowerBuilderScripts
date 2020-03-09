d54c1fbf07cb172fccd27f22634c74a8e6e6e037
Flibielt <hcsabi98@gmail.com>
2020-03-09 11:50:03 +0100

    Add constructor to dynamic button

---
+++ constructor.py
@@ -0,0 +1,78 @@
+import codecs
+import os
+
+constProp = {""}
+dynamicButton = False
+dynamicButtonProp = False
+skipLine = False
+constructorExist = False
+
+for root, dirs, files in os.walk("proba", topdown=False):
+   for name in files:
+        fileName = os.path.join(root, name)
+        print(fileName)
+        outFile = "out" + fileName[fileName.find("\\"):]
+        os.makedirs(os.path.dirname(outFile), exist_ok=True)
+        dynamicButton = False
+        with codecs.open(fileName, encoding='utf8') as f:
+            for line in f:
+                if " from u_dynamic_button within ":
+                    dynamicButton = True
+                    dynamicButtonProp = True
+                
+                if dynamicButtonProp == True:
+                    if "textsize" in line.lower() and "=" in line:
+                        constProp.add("this.set_textsize(" + line[line.find("=") + 1:].strip() + ")")
+                        skipLine = True
+                    elif "string powertiptext" in line.lower() and "=" in line:
+                        constProp.add("this.add_powertip(" + line[line.find("=") + 1:].strip() + ")")
+                        skipLine = True
+                    elif "string text" in line.lower() and "=" in line:
+                        constProp.add("this.set_text(" + line[line.find("=") + 1:].strip() + ")")
+                        skipLine = True
+                    elif "string tag" in line.lower():
+                        constProp.add("this.set_tag(" + line[line.find("=") + 1:].strip() + ")")
+                        skipLine = True
+                    elif "boolean default" in line.lower():
+                        constProp.add("this.set_default(" + line[line.find("=") + 1:].strip() + ")")
+                        skipLine = True
+                    elif "boolean cancel" in line.lower():
+                        constProp.add("this.set_cancel(" + line[line.find("=") + 1:].strip() + ")")
+                        skipLine = True
+                    elif "end type" in line.lower():
+                        dynamicButtonProp = False
+                
+                if dynamicButton and line.strip().startswith("type") and "u_dynamic_button" not in line:
+                    dynamicButton = False
+            
+                if not skipLine:
+                    with open(outFile, "a") as f:
+                        insertSetter = False
+                        insertConst = False
+                        if dynamicButton == True:
+                            if " event " in line.lower() and "constructor" in line.lower():
+                                insertSetter = True
+                        elif dynamicButton == False and len(constProp) > 1:
+                            insertConst = True
+                        
+                        if insertSetter == True:
+                            f.write(line.rstrip())
+                            f.write("\n")
+                            for settter in constProp:
+                                f.write(settter)
+                                f.write("\n")
+                            constProp.clear()
+
+                        elif insertConst == True:
+                            f.write("event constructor;call super::constructor;")
+                            f.write("\n")
+                            for setter in constProp:
+                                f.write(setter)
+                                f.write("\n")
+                            constProp.clear()
+                            
+                            f.write("end event")
+                            f.write("\n")
+                            f.write("\n")
+                skipLine = False
+
