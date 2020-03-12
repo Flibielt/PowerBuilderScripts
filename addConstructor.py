35fd88911fc2fb7599eb6a69eba9641ae4d3cb92
Flibielt <hcsabi98@gmail.com>
2020-03-12 11:24:08 +0100

    Bug fix

--- addConstructor.py
+++ addConstructor.py
@@ -14,14 +14,9 @@ for root, dirs, files in os.walk("folderName", topdown=False):
             skipLine = False
             with codecs.open(fileName, encoding='utf8') as f:
                 for line in f:
-                    if " from u_dynamic_button " in line:
-                        button = True
-                        props.add("this.resize_inner_objects(this.width, this.height)")
-                    elif "end type" in line and " from u_dynamic_button " in lastLine:
-                        props.clear()
-                        button = False
-                    elif button:
+                    skipLine = False
 
+                    if button:
                         if "integer textsize" in line.lower() or "int textsize" in line.lower():
                             props.add("this.set_textsize(" + line[line.find("=") + 1:].strip() + ")")
                             skipLine = True
@@ -41,24 +36,44 @@ for root, dirs, files in os.walk("folderName", topdown=False):
                             props.add("this.set_cancel(" + line[line.find("=") + 1:].strip() + ")")
                             skipLine = True
 
-                        if "constructor" in lastLine.lower():
+                        if "constructor" in lastLine.lower() and ";" in line:
                             if len(props) > 0:
                                 with open(outFile, "a") as fOut:
                                     for setter in props:
                                         fOut.write(setter)
                                         fOut.write("\n")
+                                props.clear()
                                 button = False
-                        elif " type " in line and " from " in line:
+                        elif "type" in line and " from " in line:
                             if len(props) > 0:
-                                with open(outFile, "a") as fOut:
-                                    fOut.write("event constructor;call super::constructor;")
-                                    fOut.write("\n")
-                                    for setter in props:
-                                        fOut.write(setter)
+                                validSetters = False
+                                for setter in props:
+                                    if len(setter) > 5:
+                                        validSetters = True
+                                if validSetters:
+                                    with open(outFile, "a") as fOut:
+                                        fOut.write("event constructor;call super::constructor;")
                                         fOut.write("\n")
-                                    fOut.write("end event")
-                                    fOut.write("\n")
-                                    fOut.write("\n")
+                                        for setter in props:
+                                            fOut.write(setter)
+                                            fOut.write("\n")
+                                        fOut.write("end event")
+                                        fOut.write("\n")
+                                        fOut.write("\n")
+                            
+                            props.clear()
+                            button = False
+
+                    if "from u_dynamic_button" in line:
+                        button = True
+                        props.add("this.resize_inner_objects(this.width, this.height)")
+                    elif "end type" in line and "from u_dynamic_button" in lastLine:
+                        props.clear()
+                        button = False
+                    elif "type cb_" in line or "type pb_" in line:
+                        if "`cb_" in line or "`pb_" in line:
+                            button = True
+                            props.add("this.resize_inner_objects(this.width, this.height)")
                         
                     lastLine = line
 
@@ -66,4 +81,20 @@ for root, dirs, files in os.walk("folderName", topdown=False):
                         with open(outFile, "a") as fOut:
                             fOut.write(line.rstrip())
                             fOut.write("\n")
+            
+            if len(props) > 0 and button:
+                isValid = False
+                for setter in props:
+                    if len(setter) > 5:
+                        isValid = True
+                
+                with open(outFile, "a") as fOut:
+                    fOut.write("event constructor;call super::constructor;")
+                    fOut.write("\n")
+                    for setter in props:
+                        fOut.write(setter)
+                        fOut.write("\n")
+                    fOut.write("end event")
+                    fOut.write("\n")
+                    fOut.write("\n")
                     
