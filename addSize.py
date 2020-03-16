79d1edb663a977450e2e592e1786459e89b1e9ea
Flibielt <hcsabi98@gmail.com>
2020-03-16 13:05:13 +0100

    Added addSize.py

---
+++ addSize.py
@@ -0,0 +1,13 @@
+import codecs
+import os
+
+for root, dirs, files in os.walk("folderName", topdown=False):
+   for name in files:
+        fileName = os.path.join(root, name)
+        print(fileName)
+        outFile = "folderName_out" + fileName[fileName.find("\\"):]
+        os.makedirs(os.path.dirname(outFile), exist_ok=True)
+        button = False
+        with codecs.open(fileName, encoding='utf8') as f:
+            for line in f:
+               print(line)
