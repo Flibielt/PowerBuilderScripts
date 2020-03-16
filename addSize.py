b2d482ab166c0eba5787257d1cef095b337ba119
Flibielt <hcsabi98@gmail.com>
2020-03-16 13:17:25 +0100

    Add width and height property, if not defined

--- addSize.py
+++ addSize.py
@@ -1,6 +1,9 @@
 import codecs
 import os
 
+defaultWidth = 300
+defaultHeight = 120
+
 for root, dirs, files in os.walk("folderName", topdown=False):
    for name in files:
         fileName = os.path.join(root, name)
@@ -8,6 +11,33 @@ for root, dirs, files in os.walk("folderName", topdown=False):
         outFile = "folderName_out" + fileName[fileName.find("\\"):]
         os.makedirs(os.path.dirname(outFile), exist_ok=True)
         button = False
+        lastLine = ""
+        isWidthProp = False
+        isHeightProp = False
         with codecs.open(fileName, encoding='utf8') as f:
             for line in f:
-               print(line)
+                if button:
+                    if "integer width" in line.lower() or "int width" in line.lower():
+                        isWidthProp = True
+                    elif "integer height" in line.lower() or "int height" in line.lower():
+                        isHeightProp = True
+                    elif "end type" in line.lower():
+                        if " from u_dynamic_button within " not in lastLine.lower():
+                            if not isWidthProp:
+                                with open(outFile, "a") as fOut:
+                                    fOut.write("integer width = " + str(defaultWidth))
+                                    fOut.write("\n")
+                            elif not isHeightProp:
+                                with open(outFile, "a") as fOut:
+                                    fOut.write("integer height = " + str(defaultHeight))
+                                    fOut.write("\n")
+                        button = False
+                        isWidthProp = False
+                        isHeightProp = False
+                elif " from u_dynamic_button within " in line:
+                    button = True
+                
+                with open(outFile, "a") as fOut:
+                    fOut.write(line.rstrip())
+                    fOut.write("\n")
+                    
