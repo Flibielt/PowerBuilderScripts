de7328bd54637fb6e7f6a870c6020cdeddf37a13
Csaba Hermann <hcsabi98@gmail.com>
2020-03-17 14:00:22 +0100

    Set new font
    SVN folder should be ignored

--- setFontFace.py
+++ setFontFace.py
@@ -1,22 +1,24 @@
 import codecs
 import os
 
-oldFonts = {"MS Sans Serif", "Courier New CE", "Arial CE", "Arial", "Microsoft Sans Serif", "@Arial Unicode MS", "Arial Unicode MS", "Tahoma", "Garamond", "Courier New"}
+oldFonts = {"MS Sans Serif", "Courier New CE", "Arial CE", "Arial", "Microsoft Sans Serif", "@Arial Unicode MS",
+            "Arial Unicode MS", "Tahoma", "Garamond", "Courier New", "Fixedsys"}
 newFont = "DINPro-Medium"
-# Webdings, Windings, Fixedsys  ??
+# Webdings, Windings should be ignored
 
 for root, dirs, files in os.walk("folderName", topdown=False):
-   for name in files:
+    for name in files:
         fileName = os.path.join(root, name)
-        print(fileName)
-        outFile = "folderName_out" + fileName[fileName.find("\\"):]
-        os.makedirs(os.path.dirname(outFile), exist_ok=True)
-        with codecs.open(fileName, encoding='utf8') as f:
-            for line in f:
-                for font in oldFonts:
-                    if font in line:
-                        line.replace(font, newFont)
-                
-                with open(outFile, "a") as fOut:
-                    fOut.write(line.rstrip())
-                    fOut.write("\n")
+        if "svn" not in fileName:
+            print(fileName)
+            outFile = "folderName_out" + fileName[fileName.find("\\"):]
+            os.makedirs(os.path.dirname(outFile), exist_ok=True)
+            with codecs.open(fileName, encoding='utf8') as f:
+                for line in f:
+                    for font in oldFonts:
+                        if font in line:
+                            line.replace(font, newFont)
+
+                    with open(outFile, "a") as fOut:
+                        fOut.write(line.rstrip())
+                        fOut.write("\n")
