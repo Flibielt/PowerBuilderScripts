0fcf215980bbd40ef234f8d2e4b25cbcd7e2470c
Csaba Hermann <hcsabi98@gmail.com>
2020-03-17 09:25:21 +0100

    .svn folder should be ignored

--- addSize.py
+++ addSize.py
@@ -14,30 +14,31 @@ for root, dirs, files in os.walk("folderName", topdown=False):
         lastLine = ""
         isWidthProp = False
         isHeightProp = False
-        with codecs.open(fileName, encoding='utf8') as f:
-            for line in f:
-                if button:
-                    if "integer width" in line.lower() or "int width" in line.lower():
-                        isWidthProp = True
-                    elif "integer height" in line.lower() or "int height" in line.lower():
-                        isHeightProp = True
-                    elif "end type" in line.lower():
-                        if " from u_dynamic_button within " not in lastLine.lower():
-                            if not isWidthProp:
-                                with open(outFile, "a") as fOut:
-                                    fOut.write("integer width = " + str(defaultWidth))
-                                    fOut.write("\n")
-                            if not isHeightProp:
-                                with open(outFile, "a") as fOut:
-                                    fOut.write("integer height = " + str(defaultHeight))
-                                    fOut.write("\n")
-                        button = False
-                        isWidthProp = False
-                        isHeightProp = False
-                elif " from u_dynamic_button within " in line or "`cb_" in line or "`pb_" in line:
-                    button = True
+        if ".svn" in fileName:
+            with codecs.open(fileName, encoding='utf8') as f:
+                for line in f:
+                    if button:
+                        if "integer width" in line.lower() or "int width" in line.lower():
+                            isWidthProp = True
+                        elif "integer height" in line.lower() or "int height" in line.lower():
+                            isHeightProp = True
+                        elif "end type" in line.lower():
+                            if " from u_dynamic_button within " not in lastLine.lower():
+                                if not isWidthProp:
+                                    with open(outFile, "a") as fOut:
+                                        fOut.write("integer width = " + str(defaultWidth))
+                                        fOut.write("\n")
+                                if not isHeightProp:
+                                    with open(outFile, "a") as fOut:
+                                        fOut.write("integer height = " + str(defaultHeight))
+                                        fOut.write("\n")
+                            button = False
+                            isWidthProp = False
+                            isHeightProp = False
+                    elif " from u_dynamic_button within " in line or "`cb_" in line or "`pb_" in line:
+                        button = True
 
-                lastLine = line
-                with open(outFile, "a") as fOut:
-                    fOut.write(line.rstrip())
-                    fOut.write("\n")
+                    lastLine = line
+                    with open(outFile, "a") as fOut:
+                        fOut.write(line.rstrip())
+                        fOut.write("\n")
